b0108cc7828453ec4f8699a926d852a530cc32ab
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2019-08-11 22:29:23 +0900

    Update quadtreereverse.cpp

--- algospot/quadtreereverse.cpp
+++ algospot/quadtreereverse.cpp
@@ -1,40 +1,37 @@
-#include<iostream>
-#include<string>
-#include<vector>
-
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-/*
- * 시간복잡도 : O(N)
- * 공간복잡도 : O(N)
- */
+int tc;
+string quad;
+
+void InputData(void) {
+	cin>>quad;
+}
 
 string solve(string::iterator& it) {
-
 	char head = *it;
-	it++;
-
-	if (head == 'w' || head == 'b') {
-		return string(1, head); // char to string
+	++it;
+	if(head == 'b' || head == 'w') {
+		return string(1,head);
 	}
 	
-	vector<string> arr(4);
-	for (int i = 0; i < 4; i++) {
-		arr[i] = solve(it);
-	}
-
-	// if head not 'w' or 'b' -> must 'x'.
-	return 'x' + arr[2] + arr[3] + arr[0] + arr[1];
+	string upleft = solve(it);
+	string upright = solve(it);
+	string downleft = solve(it);
+	string downright = solve(it);
+	return string("x") + downleft + downright + upleft + upright;
 }
 
-int main() {
-	int N;
-	cin >> N;
-	while (N--) {
-		string input;
-		cin >> input;
-		string::iterator it = input.begin();
-		cout << solve(it) << endl;
+int main(void) {
+	cin>>tc;
+	while(tc--) {
+		InputData();
+		string::iterator itr;
+		itr = quad.begin();
+		cout<<solve(itr)<<endl;
+		
 	}
 	return 0;
-}
+} 
