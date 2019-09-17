a6d3d3807934c5bbc43c65005fa020107f8a80ec
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2019-09-17 15:57:10 +0900

    Update temp1.cpp

--- algospot/temp1.cpp
+++ algospot/temp1.cpp
@@ -8,6 +8,7 @@ using namespace std;
 const int MOD = 1000000007;
 int K;
 int M;
+int tc;
 string word[15];
 int sum[15][15];
 int cache[15][1<15]
@@ -29,6 +30,18 @@ int solve(int last, int used) {
 	return ret;
 }
 
+string reconstruct(int last, int used) {
+	if(used == (i<<K - 1)) {
+		return ""; 
+	}
+	int maxsum = solve(last,used);
+	for(int next = 0; next < K; next++) {
+		if(maxsum == solve(next,used)) {
+			return word[next].substr(sum[last][next]) + reconstruct(next, used + 1<<next);
+		}
+	}
+}
+
 int wordsum(const string s1, const string s2) {
 	for(int len = min(s1.size(),s2.size()); len > 0; len--) {
 		if(s1.substr(s1.size() - len) == s2.substr(0,len)) {
@@ -37,13 +50,31 @@ int wordsum(const string s1, const string s2) {
 	}
 }
 int main(void) {
-	for(int i = 0; i < K; i++) {
-		for(int j = i + 1; j < K; j++) {
-			if(word[i].find(word[j]) != -1) {
-				word[j] = word[K];
-			}
-			K--;
+	cin>>tc;
+	while(tc--) {
+		cin>>K;
+		for(int i = 0; i < K; i++) {
+			cin>>word[i];
 		}
-	}
-}
 
+		for(int i = 0; i < K; i++) {
+			for(int j = i + 1; j < K; j++) {
+				if(word[i].find(word[j]) != -1) {
+					word[j] = word[K];
+				}
+				K--;
+			}
+		}
+		word[K] = "";
+		for(int i = 0; i < K; i++) {
+			for(int j = 0; j < K; j++) {
+				sum[i][j] = wordsum(word[i],word[j]);
+			}
+			
+		}
+
+		
+		
+	}
+	
+}
