a7fe98c63cd58b9b49644cb63139cf9b2f5fa77b
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2019-11-11 16:21:38 +0900

    Create p3.cpp

---
+++ project/p3.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class People {
+	string name;
+	string addr;
+	int age;
+public:
+	People(string n, string add, int a) : name(n), addr(add), age(a) {}
+	People(const People& src) : name(src.name), addr(src.addr), age(src.age) {
+		cout<<copy<<endl;
+	}
+	People(People&& src) : name(move(src.name)), addr(move(src.addr)), age(a) { 
+		cout<<move<<endl;
+	}
+	People& operator=(const People& src) {
+		cout<<copy=<<endl;
+		name = src.name;
+		addr = src.addr;
+		age = src.age;
+		return *this;
+	}
+	People& operator=(People&& src) {
+		cout<<move=<<endl;
+		name = move(src.name);
+		addr = move(src.addr);
+		age = move(src.age);
+	}
+};
+
+int main() {
+	People p1{"홍길동", "서울", 20};
+	People p2 = p1;
+	People p3 = move(p1);
+}
