9517c34c85f78f87dde418c711ccf3bcf2636745
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2019-11-18 16:21:42 +0900

    Update PINBALL.cpp

--- algospot/PINBALL.cpp
+++ algospot/PINBALL.cpp
@@ -110,333 +110,186 @@ bool parallelSegments(vector2 a, vector2 b, vector2 c, vector2 d, vector2 &p)
 {
         if (b < a)
                swap(a, b);
-
         if (d < c)
-
                swap(c, d);
-
         //한 직선 위에 없거나 두 선분이 겹치지 않는 경우를 우선 걸러낸다
-
         if (ccw(a, b, c) != 0 || b < c || d < a)
-
                return false;
-
         //두 선분은 확실히 겹친다. 교차점을 하나 찾자
-
         if (a < c)
-
                p = c;
-
         else
-
                p = a;
-
         return true;
-
 }
-
  
-
 //p가 (a, b)를 감싸면서 각 변이 x, y축에 평행한 최소 사각형 내부에 있는지 확인
-
 //a, b, p는 일직선 상에 있다고 가정
-
 bool inBoundingRectangle(vector2 p, vector2 a, vector2 b)
-
 {
-
         if (b < a)
-
                swap(a, b);
-
         return p == a || p == b || (a < p &&p < b);
-
 }
-
  
-
 //(a, b) 선분과 (c, d) 선분의 교점을 p에 반환
-
 //교점이 여러개일 경우 아무 점이나 반환
-
 //두 선분이 교차하지 않을 경우 false 반환
-
 bool segmentIntersection(vector2 a, vector2 b, vector2 c, vector2 d, vector2 &p)
-
 {
-
         //두 직선이 평행인 경우를 우선 예외 처리
-
         if (!lineIntersection(a, b, c, d, p))
-
                return parallelSegments(a, b, c, d, p);
-
         //p가 두 선분에 포함되어 있는 경우에만 참을 반환
-
         return inBoundingRectangle(p, a, b) && inBoundingRectangle(p, c, d);
-
 }
-
  
-
 //두 선분의 교차 여부를 좀 더 간단하게 확인
-
 bool segmentIntersects(vector2 a, vector2 b, vector2 c, vector2 d)
-
 {
-
         double ab = ccw(a, b, c)*ccw(a, b, d);
-
         double cd = ccw(c, d, a)*ccw(c, d, b);
-
         //두 선분이 한 직선 위에 있거나 끝점이 겹치는 경우
-
         if (ab == 0 && cd == 0)
-
         {
-
                if (b < a)
-
                        swap(a, b);
-
                if (d < c)
-
                        swap(c, d);
-
                return !(b < c || d < a);
-
         }
-
         return ab <= 0 && cd <= 0;
-
 }
-
  
-
 //점과 선 사이의 거리를 계산하는 함수 pointToLine()의 구현
-
 //점 p에서 (a, b) 직선에 내린 수선의 발을 구한다
-
 vector2 perpendicularFoot(vector2 p, vector2 a, vector2 b)
-
 {
-
         return a + (p - a).project(b - a);
-
 }
-
  
-
 //점 p와 (a, b) 직선 사이의 거리를 구한다
-
 double pointToLine(vector2 p, vector2 a, vector2 b)
-
 {
-
         return (p - perpendicularFoot(p, a, b)).norm();
-
 }
-
 //여기까지가 vector2 관련 함수
-
 //이후부터 PINBALL.cpp
-
  
-
 int obstacleNum;
-
 vector<vector2> center; //중심
-
 vector<int> radius; //반지름
-
  
-
 //2차 방정식 ax^2+bx+c=0의 모든 실근을 크기 순서대로 반환
-
 vector<double> solve2(double a, double b, double c)
-
 {
-
         double d = b * b - 4 * a*c;
-
         //해가 없는 경우
-
         if (d < -EPSILON)
-
                return vector<double>();
-
         //중근
-
         if (d < EPSILON)
-
                return vector<double>(1, -b / (2 * a));
-
         vector<double> result;
-
         result.push_back((-b - sqrt(d)) / (2 * a));
-
         result.push_back((-b + sqrt(d)) / (2 * a));
-
         return result;
-
 }
-
  
 
-
-//here에 있던 공이 1초마다 dir만큼 굴러갈 때, center를 중심으로 하고
-
+/here에 있던 공이 1초마다 dir만큼 굴러갈 때, center를 중심으로 하고
 //반지름 radius인 장애물과 몇 초 후에 충돌하는지 반환
-
 //충돌하지 않을 경우 '아주 큰 값' INFTY를 반환
-double hitCircle(vector2 here, vector2 dir, vector2 center, int radius)
-
+ouble hitCircle(vector2 here, vector2 dir, vector2 center, int radius)
 {
-
         //f(t)=here+t*dir;
-
         //(center-f(t))*(center-f(t)) =
-
         //((dir*dir)*t^2) + (2*(here-center)*dir)*t + (center*center + here*here - 2*(center*here) - radius*radius)
-
         double a = dir.dot(dir);
-
         double b = 2 * dir.dot(here - center);
-
         double c = center.dot(center) + here.dot(here) - 2 * here.dot(center) - radius * radius;
-
  
-
         vector<double> sols = solve2(a, b, c);
-
         if (sols.empty() || sols[0] < EPSILON)
-
                return INFTY;
-
         return sols[0];
-
 }
-
  
-
 //here에 있던 공이 dir 방향으로 굴러와 center를 중심으로 하는 장애물에서
-
 //contact 위치에서 충돌했을 때 공의 새로운 방향 반환
-
 vector2 reflect(vector2 dir, vector2 center, vector2 contact)
-
 {
-
         return (dir - dir.project(contact - center) * 2).normalize();
-
 }
-
  
-
 //공의 현재 위치와 방향이 주어질 때 최대 10번의 충돌 출력
-
 void simulate(vector2 here, vector2 dir)
-
 {
-        //방향은 항상 단위 벡터로 저장
-        dir = dir.normalize();
-        int hitCount = 0;
-        while (hitCount < 100)
-        {
-               //이번에 충돌한 장애물을 찾는다
-               int circle = -1;
-               double time = INFTY * 0.5;
-               //각 장애물을 순회하며 가장 먼저 만나는 장애물을 찾는다
-               for (int i = 0; i < obstacleNum; i++)
-               {
-                       double candidate = hitCircle(here, dir, center[i], radius[i] + 1);
-                       if (candidate < time)
-                       {
-                              time = candidate;
-                              circle = i;
-                       }
-               }
-               //더 이상 장애물에 충돌하지 않고 게임판을 벗어나는 경우
-               if (circle == -1)
-                       break;
-               //부딪히는 장애물의 번호를 출력
-               if (hitCount++)
-                       cout << " ";
-               cout << circle;
-               //공의 새 위치를 계산
-               vector2 contact = here + dir * time;
-               //부딪힌 위치와 새 방향으로 here과 dir 변경
-               dir = reflect(dir, center[circle], contact);
-               here = contact;
-        }
-        cout << endl;
-
+       //방향은 항상 단위 벡터로 저장
+       dir = dir.normalize();
+       int hitCount = 0;
+       while (hitCount < 100)
+       {
+              //이번에 충돌한 장애물을 찾는다
+              int circle = -1;
+              double time = INFTY * 0.5;
+              //각 장애물을 순회하며 가장 먼저 만나는 장애물을 찾는다
+              for (int i = 0; i < obstacleNum; i++)
+              {
+                      double candidate = hitCircle(here, dir, center[i], radius[i] + 1);
+                      if (candidate < time)
+                      {
+                             time = candidate;
+                             circle = i;
+                      }
+              }
+              //더 이상 장애물에 충돌하지 않고 게임판을 벗어나는 경우
+              if (circle == -1)
+                      break;
+              //부딪히는 장애물의 번호를 출력
+              if (hitCount++)
+                      cout << " ";
+              cout << circle;
+              //공의 새 위치를 계산
+              vector2 contact = here + dir * time;
+              //부딪힌 위치와 새 방향으로 here과 dir 변경
+              dir = reflect(dir, center[circle], contact);
+              here = contact;
+       }
+       cout << endl;
 }
- 
-int main(void)
 
+nt main(void)
 {
-
         int test_case;
-
         cin >> test_case;
-
         if (test_case < 1 || test_case>50)
-
                exit(-1);
-
  
-
         for (int i = 0; i < test_case; i++)
-
         {
-
                center.clear();
-
                radius.clear();
-
                int x, y, dx, dy;
-
                cin >> x >> y >> dx >> dy >> obstacleNum;
-
                if (x < 0 || x>100 || y < 0 || y>100)
-
                        exit(-1);
-
                if (dx < -10 || dx>10 || dy < -10 || dy>10)
-
                        exit(-1);
-
  
-
                vector2 here(x, y);
-
                vector2 dir(dx, dy);
-
               
-
                for (int j = 0; j < obstacleNum; j++)
-
                {
-
                        int cx, cy;
-
                        cin >> cx >> cy;
-
                        center.push_back(vector2(cx, cy));
-
                        int r;
-
                        cin >> r;
-
                        radius.push_back(r);
-
                }
-
                simulate(here, dir);
-
         }
-
         return 0;
-
 }
