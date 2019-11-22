720e077cb54832215cf7e43e5d217a4454de0b5b
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2019-11-22 10:55:25 +0900

    Update pack.cpp

--- algospot/pack.cpp
+++ algospot/pack.cpp
@@ -12,29 +12,27 @@ int cache[1001][101];
 string name[101];
 int value[101];
 int weight[101];
-vector<string> ans;
+
 int pack(int item, int w) {
 
 	if(item == N) return 0;
 	int& ret = cache[w][item];
 	if(ret != -1) return ret;
-	ret = 0;
 	ret = pack(item + 1, w);
 	if(w >= weight[item]){
 		ret = max(ret, pack(item + 1 , w - weight[item]) + value[item]) ;
 	}
 	return ret;
-	
 }
 
-void reconstruct(int item,int w) {
+void reconstruct(int item,int w, vector<string>& ans) {
 	if(item == N) return ;
-	if(pack(item + 1, w) == pack(item + 1, w-weight[item])) {
-		reconstruct(item + 1, w);		
+	if(pack(item, w) == pack(item + 1, w)) {
+		reconstruct(item + 1, w, ans);		
 	}
 	else {
 		ans.push_back(name[item]);
-		reconstruct(item + 1, w-weight[item]);
+		reconstruct(item + 1, w-weight[item], ans);
 			
 	}
 }
@@ -45,14 +43,14 @@ int main(int argc, char** argv) {
 		memset(cache,-1,sizeof(cache));
 		memset(value,0,sizeof(value));
 		memset(weight,0,sizeof(weight));
-		
+		vector<string> ans;
 		cin>>N>>W;
 		for(int i = 0; i < N; i++) {
 			cin>>name[i]>>weight[i]>>value[i];
 		}
-		cout<<pack(0,W)<<" ";
-		reconstruct(0,W);
-		cout<<ans.size()<<endl;
+		//cout<<pack(0,W)<<" ";
+		reconstruct(0,W,ans);
+		cout<<pack(0,W)<<" "<<ans.size()<<endl;
 		for(int i = 0; i < W; i++) {
 			cout<<ans[i]<<endl;
 		}
@@ -61,4 +59,3 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-// name 정리가 잘 안된다...
