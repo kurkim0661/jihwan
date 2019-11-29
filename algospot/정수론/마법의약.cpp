a7666f540644fd97ccb7e29d62a8395fd29d3cf4
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2019-11-29 10:24:33 +0900

    Update 마법의약.cpp

--- algospot/정수론/마법의약.cpp
+++ algospot/정수론/마법의약.cpp
@@ -39,26 +39,39 @@ vector<int> solve(void) {
 }
 
 
-int main(void) {
-	vector<int> ret;
-	cin>>TC;
-	while(TC--) {
-		cin>>N;
-		for(int i = 0; i < N; i ++) {
-			cin>>need[i];
-		}
-		for(int i = 0; i < N; i ++) {
-			cin>>aleady[i];
-		}
-		base = maxpill();
-		for(int i = 0; i < N; i++) {
-			if(aleady[i]/need[i] == maxratio)
-		}
-		ret = solve();
-		for(int i = 0; i < ret.size(); i++) {
-			printf("%d ",ret[i]);
-		}
-		cout<<endl;
-		
-	}
+nt main(void)
+
+{
+        int test_case;
+        cin >> test_case;
+        if (test_case < 1 || test_case>50)
+               exit(-1);
+ 
+        for (int i = 0; i < test_case; i++)
+        {
+               int ingredientNum;
+               vector<int> recipe, put;
+               cin >> ingredientNum;
+               if (ingredientNum < 1 || ingredientNum>200)
+                       exit(-1);
+ 
+               for (int j = 0; j < ingredientNum; j++)
+               {
+                       int temp;
+                       cin >> temp;
+                       recipe.push_back(temp);
+               }
+               for (int j = 0; j < ingredientNum; j++)
+               {
+                       int temp;
+                       cin >> temp;
+                       put.push_back(temp);
+               }
+               //vector<int> result = solveSimulation(recipe, put);
+               vector<int> result = solve(recipe, put);
+               for (int j = 0; j < result.size(); j++)
+                       cout << result[j] << " ";
+               cout << endl;
+        }
+        return 0;
 }
