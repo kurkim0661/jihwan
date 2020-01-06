f564ba0329d83c7e525c6e294391c3132659041b
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2020-01-06 13:49:34 +0900

    Update 빌더패턴.cpp

--- degine/빌더패턴.cpp
+++ degine/빌더패턴.cpp
@@ -59,3 +59,175 @@ int main(void)
 	delete pDirector;
 	return 0;
 }
+///////////////////
+예제
+#include <iostream>
+#include <string>
+using namespace std;
+class CEngine
+{
+public:
+	CEngine(int mph) : mMPH(mph){}
+	int GetMPH() const
+	{
+		return mMPH;
+	}
+private:
+	int mMPH;
+};
+
+class CWheel
+{
+public:
+	CEWheel(int size) : mSize(size){}
+	int GetSize() const
+	{
+		return mSize;
+	}
+private:
+	int mSize;
+
+};
+
+class CBody
+{
+public:
+	CBody(string shape) : mShape(shape) {}
+	string GetShape() const
+	{
+		return mShape;
+	}
+private:
+	string mShape:
+};
+
+class Car
+{
+public:
+	Car() : mEngine(nullptr), mWheel(nullptr), mBody(nullptr){}
+	~Car()
+	{
+		if(mEngine)
+			delete mEngine;
+		if(mWheel)
+			delete mWheel;
+		if(mBody)
+			delete mBody;
+	}
+	void SetEngine(CEngine* engine)
+	{
+		mEngine = engine;
+	}
+	void SetWheel(CWheel* wheel)
+	{
+		mWheel = wheel;
+	}
+	void SetBody(CBody* body)
+	{
+		mBody = body;
+	}
+	void SetManufacture(string m)
+	{
+		mManufacture = m;
+	}
+	void print() const
+	{
+		cout<<"제조 회사 : "<<mManufacture<<endl;
+		cout<<"엔진 마력 : "<<mEngine->GetMPH()<<endl;
+		cout<<"휠 사이즈 : "<<mWheel->GetSize()<<endl;
+		cout<<"외형 타입 : "<<mBody->GetShape()<<endl;
+		
+	}
+private:
+	CEngine* mEngine;
+	CWheel* mWheel;
+	CBody* mBody;
+	string mManufacture;
+};
+
+class Builder
+{
+public:
+	virtual CEngine* GetEngine() = 0;
+	virtual CWheel* GetWheel() = 0;
+	virtual CBody* GetBody() = 0;
+	virtual string GetManufacture() = 0;
+};
+
+class FordBuilder : public Builder
+{
+public:
+	CEngine* GetEngine() override
+	{
+		return new CEngine(150);
+	}
+	CWheel* GetWheel() override
+	{
+		return new CWheel(18);
+	}
+	CBody* GetBody() override 
+	{
+		return new CBody("hatch back");
+	}
+	string GetManufacture() override
+	{
+		return "Ford";
+	}
+};
+
+class JeepBuilder : public Builder
+{
+public:
+	CEngine* GetEngine() override
+	{
+		return new CEngine(120);
+	}
+	CWheel* GetWheel() override
+	{
+		return new CWheel(29);
+	}
+	CBody* GetBody() override 
+	{
+		return new CBody("SUV");
+	}
+	string GetManufacture() override
+	{
+		return "Jeep";
+	}
+};
+
+class Director
+{
+public:
+	Director(Builder* builder) : m_builder(builder) {}
+	~Director()
+	{
+		if(m_builder)
+		{
+			delete m_builder;
+		}
+	}
+	Car* construct(void)
+	{
+		Car* car = new Car;
+		car->SetBody(m_builder->GetBody());
+		Car->SetEngine(m_builder->GetEngine());
+		Car->SetManufacture(m_builder->GetManufacture());
+		Car->SetWheel(m_builder->GetWheel());
+
+		return car;
+	}
+private:
+	Builder* m_builder;
+};
+
+int main(void)
+{
+	Director* a_director = new Director(FordBuilder());
+	Car* car = a_director->construct();
+	car->print();
+
+	delete car;
+	delete a_director;
+	return 0;
+}
