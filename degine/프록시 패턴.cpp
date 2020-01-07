40ea18f46cfdd1281136357a341848e5a320436c
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2020-01-07 13:50:19 +0900

    Update 프록시 패턴.cpp

--- degine/프록시 패턴.cpp
+++ degine/프록시 패턴.cpp
@@ -39,3 +39,69 @@ public:
 private:
 	RealSubject* mRealSubject;
 };
+
+///////////////예제
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Image
+{
+public:
+	virtual void ShowImage() = 0;
+};
+
+class RealImage : public Image
+{
+public:
+	RealImage(const char* name)
+		:mFile(name){}
+	void LoadImage()
+	{
+		cout<<"disk from loading file: "<<mFile.c_str()<<endl;
+	}
+	void ShowImage()
+	{
+		cout<<"Show image : "<<mFile.c_str()<<endl;
+	}
+private:
+	string mFile;
+};
+
+class ProxyImage : public Image
+{
+public:
+	ProxyImage(const char* name)
+		:mFile(name), m_Image(nullptr)
+	{}
+	~ProxyImage()
+	{
+		if(m_Image)
+		{
+			delete m_Image;
+		}
+	}
+	void ShowImage()
+	{
+		if(!m_Image)
+		{
+			m_Image = new RealImage(mFile.c_str());
+			m_Image->LoadImage();
+		}
+		m_Image->ShowImage();
+	}
+private:
+	RealImage* m_Image;
+	string mFile;
+};
+
+int main(void)
+{
+	Image* p_Image = new ProxyImage("image.png");
+	p_Image->ShowImage();
+	p_Image->ShowImage();
+	p_Image->ShowImage();
+	delete p_Image;
+
+	return 0;
+}
